92266cd05ce93986892f0ce6235e96e12823a15e
George Brighton <george@gebn.co.uk>
2019-08-26 01:38:29 +0100

    Add use of the Get SDR Repository Info command to its doc

--- pkg/ipmi/get_sdr_repository_info.go
+++ pkg/ipmi/get_sdr_repository_info.go
@@ -13,7 +13,9 @@ import (
 
 // GetSDRRepositoryInfoRsp represents the response to a Get SDR Repository Info
 // command, specified in section 27.9 and 33.9 of IPMI v1.5 and v2.0
-// respectively.
+// respectively. This command is useful for finding out how many SDRs are in the
+// repository, as well as the version, which indicates how to decode the Get SDR
+// command response (in practice, the format does not change between versions).
 type GetSDRRepositoryInfoRsp struct {
 	layers.BaseLayer
 
