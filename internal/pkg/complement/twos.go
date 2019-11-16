84cbac4255e5211fbea8b261cc1cd476e1799c5d
George Brighton <george@gebn.co.uk>
2019-11-16 00:09:23 +0000

    Inline BigEndian.Uint16()

--- internal/pkg/complement/twos.go
+++ internal/pkg/complement/twos.go
@@ -1,17 +1,14 @@
 package complement
 
-import (
-	"encoding/binary"
-)
-
 // Twos parses two's complement numbers of up to 16 bits into a native integer.
 // The input is two bytes in big-endian order, and the number of bits the binary
 // representation is expected to be (0 through 16). More significant bits above
 // this must be 0, e.g. Twos([...]byte{0b000000xx, 0bxxxxxxxx}, 10).
 func Twos(bigEndian [2]byte, bits uint8) int16 {
 	// this abstracts away the endian-ness of the platform; big-endian only
-	// refers to the byte order of the input
-	numerical := binary.BigEndian.Uint16(bigEndian[:])
+	// refers to the byte order of the input. It is identical to
+	// binary.BigEndian.Uint16(), but avoids creating a slice.
+	numerical := uint16(bigEndian[1]) | uint16(bigEndian[0])<<8
 
 	// sign extend to 16 bits
 	// (https://graphics.stanford.edu/~seander/bithacks.html, "Sign extending
