026a9b4c4cfd12455c11c3e34151d8e6aaafc4ad
Pedro Fialho <pfialho@nvidia.com>
2024-01-28 09:01:58 -0500

    use functional options pattern

--- bmc.go
+++ bmc.go
@@ -23,14 +23,26 @@ var (
 	namespace = "bmc"
 )
 
+type Config struct {
+	Timeout time.Duration
+}
+
+type OptionFunc func(c *Config)
+
+func WithTimeout(timeout time.Duration) OptionFunc {
+	return func(c *Config) {
+		c.Timeout = timeout
+	}
+}
+
 // Dial is currently an alias for DialV2. When IPMI v1.5 is implemented, this
 // will query the BMC for IPMI v2.0 capability. If it supports IPMI v2.0, a
 // V2SessionlessTransport will be returned, otherwise a V1SessionlessTransport
 // will be returned. If you know the BMC's capabilities, or need a specific
 // feature (e.g. DCMI), use the DialV*() functions instead, which expose
 // additional information and functionality.
-func Dial(_ context.Context, addr string) (SessionlessTransport, error) {
-	return DialV2(addr)
+func Dial(_ context.Context, addr string, options ...OptionFunc) (SessionlessTransport, error) {
+	return DialV2(addr, options...)
 }
 
 // DialV2 establishes a new IPMI v2.0 connection with the supplied BMC. The
@@ -38,11 +50,7 @@ func Dial(_ context.Context, addr string) (SessionlessTransport, error) {
 // Use this if you know the BMC supports IPMI v2.0 and/or require DCMI
 // functionality. Note v4 is preferred to v6 if a hostname is passed returning
 // both A and AAAA records.
-func DialV2(addr string) (*V2SessionlessTransport, error) {
-	return DialV2WithCustomTimeout(addr, 1*time.Second)
-}
-
-func DialV2WithCustomTimeout(addr string, timeout time.Duration) (*V2SessionlessTransport, error) {
+func DialV2(addr string, options ...OptionFunc) (*V2SessionlessTransport, error) {
 	v2ConnectionOpenAttempts.Inc()
 	t, err := newTransport(addr)
 	if err != nil {
@@ -50,13 +58,20 @@ func DialV2WithCustomTimeout(addr string, timeout time.Duration) (*V2Sessionless
 		return nil, err
 	}
 	v2ConnectionsOpen.Inc()
-	return newV2SessionlessTransport(t, timeout), nil
+
+	c := &Config{
+		Timeout: 1 * time.Second,
+	}
+	for _, option := range options {
+		option(c)
+	}
+	return newV2SessionlessTransport(t, c), nil
 }
 
-func newV2SessionlessTransport(t transport.Transport, timeout time.Duration) *V2SessionlessTransport {
+func newV2SessionlessTransport(t transport.Transport, c *Config) *V2SessionlessTransport {
 	return &V2SessionlessTransport{
 		Transport:     t,
-		V2Sessionless: newV2Sessionless(t, timeout),
+		V2Sessionless: newV2Sessionless(t, c.Timeout),
 	}
 }
 
